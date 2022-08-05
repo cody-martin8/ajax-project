36b74a9ec450efcba5cd8e70ce123e3fc11c5777
Cody Martin <12martincody@gmail.com>
2022-08-05 01:57:40 +0000

    Completed incorporation of API functionality and building DOM trees.

---
+++ js/main.js
@@ -0,0 +1,142 @@
+// Changing view from Search Page to Filters Page
+
+var $searchPage = document.querySelectorAll('.tab');
+var $filter = document.querySelector('.filter-navigation a');
+var $filterSave = document.querySelector('.save-filters-button');
+var $filters = document.querySelectorAll('.filter-dropdown');
+var $returnButton = document.querySelector('.save-filters-button.return-button');
+var $clearFiltersButton = document.querySelector('.clear-filters');
+
+function clearFilters() {
+  for (var i = 0; i < $filters.length; i++) {
+    $filters[i].value = $filters[i][0];
+  }
+}
+
+$filter.addEventListener('click', function () {
+  $searchPage[0].className = 'container tab hidden';
+  $searchPage[1].className = 'container tab';
+});
+
+$filterSave.addEventListener('click', function () {
+  $searchPage[1].className = 'container tab hidden';
+  $searchPage[0].className = 'container tab';
+});
+
+$returnButton.addEventListener('click', function () {
+  clearFilters();
+  $searchPage[1].className = 'container tab hidden';
+  $searchPage[0].className = 'container tab';
+});
+
+$clearFiltersButton.addEventListener('click', clearFilters);
+
+// Submitting Search parameters to API
+
+var $queryText = document.querySelector('.query-text');
+var $searchButton = document.querySelector('.search-button');
+var $searchForm = document.getElementById('recipe-search-form');
+var $searchList = document.querySelector('#search-result-list');
+
+// console.log($searchList);
+
+function getRecipes(search) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=' + search.queryText + '&app_id=25ee5a1c&app_key=2e0d886c58ffcac239ddf7ae29b2d302' + search.ingredientsNumber + search.cuisineType + search.mealType + search.dishType);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+    var searchId = 0;
+    for (var i = 0; i < xhr.response.hits.length; i++) {
+      $searchList.append(renderSearchResults(xhr.response.hits[i], searchId));
+      searchId++;
+    }
+  });
+  xhr.send();
+}
+
+$searchForm.addEventListener('submit', function searchRecipes(event) {
+  event.preventDefault();
+  while ($searchList.firstChild) {
+    $searchList.removeChild($searchList.firstChild);
+  }
+  var search = {};
+  search.queryText = $searchForm[0].value;
+  if ($searchForm[2].value !== '') {
+    search.mealType = '&mealType=' + $searchForm[2].value;
+  } else {
+    search.mealType = '';
+  }
+  if ($searchForm[3].value !== '') {
+    search.ingredientsNumber = '&ingr=' + $searchForm[3].value;
+  } else {
+    search.ingredientsNumber = '';
+  }
+  if ($searchForm[4].value !== '') {
+    search.dishType = '&dishType=' + $searchForm[4].value;
+  } else {
+    search.dishType = '';
+  }
+  if ($searchForm[5].value !== '') {
+    search.cuisineType = '&cuisineType=' + $searchForm[5].value;
+  } else {
+    search.cuisineType = '';
+  }
+
+  getRecipes(search);
+})
+
+{/* <li class="search-result-item" data-entry-id="#">
+  <div class="list-row">
+    <div class="list-column">
+      <div class="search-result-image">
+        <a href="#"><img alt="bruschetta" src="https://imagesvc.meredithcorp.io/v3/"></a>
+      </div>
+      <div class="search-result-title">
+        <a href="#"><h2>Bruschetta</h2></a>
+      </div>
+    </div>
+  </div>
+</li> */}
+
+function renderSearchResults(response, searchId) {
+  var listItem = document.createElement('li');
+  listItem.className = 'search-result-item';
+  listItem.setAttribute('data-entry-id', searchId);
+
+  var listRowDiv = document.createElement('div');
+  listRowDiv.className = 'list-row';
+  listItem.appendChild(listRowDiv);
+
+  var listColumnDiv = document.createElement('div');
+  listColumnDiv.className = 'list-column';
+  listRowDiv.appendChild(listColumnDiv);
+
+  var resultImageDiv = document.createElement('div');
+  resultImageDiv.className = 'search-result-image';
+  listColumnDiv.appendChild(resultImageDiv);
+
+  var resultTitleDiv = document.createElement('div');
+  resultTitleDiv.className = 'search-result-title';
+  listColumnDiv.appendChild(resultTitleDiv);
+
+  var imageLink = document.createElement('a');
+  imageLink.setAttribute('href', '#');
+  resultImageDiv.appendChild(imageLink);
+
+  var image = document.createElement('img');
+  image.setAttribute('alt', response.recipe.label);
+  image.setAttribute('src', response.recipe.image);
+  imageLink.appendChild(image);
+
+  var titleLink = document.createElement('a');
+  titleLink.setAttribute('href', '#');
+  resultTitleDiv.appendChild(titleLink);
+
+  var title = document.createElement('h2');
+  title.textContent = response.recipe.label;
+  titleLink.appendChild(title);
+
+  return listItem;
+}
