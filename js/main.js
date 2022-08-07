9e3c18306f5f241453994e31dab505b0b9e40290
Cody Martin <12martincody@gmail.com>
2022-08-07 06:21:18 +0000

    Save Recipe button saves the recipe to the Recipe book. Use data object to populate Recipe book.

--- js/main.js
+++ js/main.js
@@ -72,11 +72,6 @@ var $searchButton = document.querySelector('.search-button');
 var $searchForm = document.getElementById('recipe-search-form');
 var $searchList = document.getElementById('search-result-list');
 
-function pullSearchList () {
-  $searchListItems = $searchList.querySelectorAll('.search-result-item');
-  console.log($searchListItems);
-}
-
 var $searchListItems;
 var searchData = [];
 
@@ -95,7 +90,6 @@ function getRecipes(search) {
       searchId++;
     }
     searchData.push(xhr.response.hits);
-    console.log(searchData);
   });
   xhr.send();
 }
@@ -195,14 +189,12 @@ var $newRecipeHeading = document.querySelector('.new-recipe-wrapper h1');
 var $newRecipeImage = document.querySelector('.new-recipe-wrapper img');
 var $newRecipeLink = document.querySelector('.new-recipe-wrapper a');
 var $newRecipeIngredients = document.getElementById('new-recipe-ingredients-list');
-
-console.log();
+var newRecipe;
 
 $searchList.addEventListener('click', function viewNewRecipe (event) {
   while ($newRecipeIngredients.firstChild) {
     $newRecipeIngredients.removeChild($newRecipeIngredients.firstChild);
   }
-  var newRecipe;
   for (var i = 0; i < searchData[0].length; i++) {
     if (i.toString() === event.target.dataset.entryId) {
       newRecipe = searchData[0][i];
@@ -215,7 +207,6 @@ $searchList.addEventListener('click', function viewNewRecipe (event) {
   for (var n = 0; n < newRecipe.recipe.ingredients.length; n++) {
     var ingredient = document.createElement('li');
     var ingredientText = document.createElement('p');
-    // console.log(newRecipe.recipe.ingredients[n]);
     ingredientText.textContent = newRecipe.recipe.ingredients[n].text;
     ingredient.appendChild(ingredientText);
     ingredient.className = 'ingredient';
@@ -224,3 +215,95 @@ $searchList.addEventListener('click', function viewNewRecipe (event) {
   $searchPage[0].className = 'container tab hidden';
   $searchPage[2].className = 'container tab';
 })
+
+// Clicking Save Recipe button
+
+var $saveRecipe = document.querySelector('.save-recipe-button');
+var $savedRecipesList = document.getElementById('saved-recipes-list');
+
+// console.log($saveRecipe);
+
+$saveRecipe.addEventListener('click', function saveRecipe() {
+  for (var i = 0; i < data.savedRecipes.length; i++) {
+    if (newRecipe.recipe === data.savedRecipes[i].recipe) {
+      $searchPage[0].className = 'container tab';
+      $searchPage[2].className = 'container tab hidden';
+      return;
+    }
+  }
+  newRecipe.savedRecipeId = data.nextSavedRecipeId;
+  data.savedRecipes.push(newRecipe);
+  data.nextSavedRecipeId++;
+  console.log(data.savedRecipes);
+  console.log(data.nextSavedRecipeId);
+  $savedRecipesList.append(renderSavedRecipes(newRecipe))
+  $searchPage[0].className = 'container tab';
+  $searchPage[2].className = 'container tab hidden';
+})
+
+
+  // < li class="saved-recipe" data - entry - id="#" >
+  //   <div class="list-row">
+  //     <div class="list-column">
+  //       <div class="saved-recipe-image">
+  //         <a href="#"><img alt="bruschetta"
+  //           src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F19%2F54165-Balsamic-Bruschetta-mfs_002.jpg"></a>
+  //       </div>
+  //       <div class="saved-recipe-title flex">
+  //         <a href="#">
+  //           <h2>Bruschetta</h2>
+  //         </a>
+  //         <a href="#" class="options-icon"><i class="fa-solid fa-ellipsis fa-xl"></i></a>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </li >
+
+function renderSavedRecipes(newRecipe) {
+  var listItem = document.createElement('li');
+  listItem.className = 'saved-recipe';
+
+  var listRowDiv = document.createElement('div');
+  listRowDiv.className = 'list-row';
+  listItem.appendChild(listRowDiv);
+
+  var listColumnDiv = document.createElement('div');
+  listColumnDiv.className = 'list-column';
+  listRowDiv.appendChild(listColumnDiv);
+
+  var recipeImageDiv = document.createElement('div');
+  recipeImageDiv.className = 'saved-recipe-image';
+  listColumnDiv.appendChild(recipeImageDiv);
+
+  var recipeTitleDiv = document.createElement('div');
+  recipeTitleDiv.className = 'saved-recipe-title flex';
+  listColumnDiv.appendChild(recipeTitleDiv);
+
+  var imageLink = document.createElement('a');
+  imageLink.setAttribute('href', '#');
+  recipeImageDiv.appendChild(imageLink);
+
+  var image = document.createElement('img');
+  image.setAttribute('alt', newRecipe.recipe.label);
+  image.setAttribute('src', newRecipe.recipe.image);
+  imageLink.appendChild(image);
+
+  var titleLink = document.createElement('a');
+  titleLink.setAttribute('href', '#');
+  recipeTitleDiv.appendChild(titleLink);
+
+  var title = document.createElement('h2');
+  title.textContent = newRecipe.recipe.label;
+  titleLink.appendChild(title);
+
+  var iconLink = document.createElement('a');
+  iconLink.setAttribute('href', '#');
+  iconLink.className = 'options-icon';
+  recipeTitleDiv.appendChild(iconLink);
+
+  var icon = document.createElement('i');
+  icon.className = 'fa-solid fa-ellipsis fa-xl';
+  iconLink.appendChild(icon);
+
+  return listItem;
+}
